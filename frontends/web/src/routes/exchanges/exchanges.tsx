3c17497a4c07083bcd624873040f2e2ca06017e6
jstrnbrg <34884458+jstrnbrg@users.noreply.github.com>
2020-05-14 13:37:29 +0200

    guide: exchanges

--- frontends/web/src/routes/exchanges/exchanges.tsx
+++ frontends/web/src/routes/exchanges/exchanges.tsx
@@ -17,6 +17,8 @@
 import { Component, h, RenderableProps } from 'preact';
 import A from '../../components/anchor/anchor';
 import Button from '../../components/forms/button';
+import { Entry } from '../../components/guide/entry';
+import { Guide } from '../../components/guide/guide';
 import { SwissMadeOpenSource } from '../../components/icon/logo';
 import { Footer, Header } from '../../components/layout';
 import { translate, TranslateProps } from '../../decorators/translate';
@@ -146,6 +148,11 @@ class Exchanges extends Component<Props, State> {
                             <div className={styles.results}>
                                 {results.length ? results : t('exchanges.nomatch')}
                             </div>
+                            <Guide>
+                                <Entry key="exchangeDescription" entry={t('guide.exchanges.description')} />
+                                <Entry key="exchangeWhichService" entry={t('guide.exchanges.whichService')} />
+                                <Entry key="accountTransactionConfirmation" entry={t('guide.exchanges.commission')} />
+                            </Guide>
                         </div>
                         <Footer>
                             <SwissMadeOpenSource />
