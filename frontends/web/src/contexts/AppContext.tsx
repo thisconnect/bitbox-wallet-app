4fad577da1f34683f17cede33f1dcd1a9af1976e
thisconnect <this@shiftcrypto.ch>
2024-09-25 15:37:42 +0200

    frontend: refactor testing into context
    Refactored isTesting to be part of the app context to ensure it is
    only fetched once and shared across components efficiently.
    
    This improves performance by reducing redundant API calls and
    simplifies the logic in components that need access to this value.

--- frontends/web/src/contexts/AppContext.tsx
+++ frontends/web/src/contexts/AppContext.tsx
@@ -24,6 +24,7 @@ type AppContextProps = {
     guideShown: boolean;
     guideExists: boolean;
     hideAmounts: boolean;
+    isTesting: boolean;
     nativeLocale: string;
     sidebarStatus: string;
     firmwareUpdateDialogOpen: boolean;
